f2ecde282c9f151d4be5573c753af9f2b4393e84
Noah Schlesinger <noah.njs@gmail.com>
2019-11-24 16:13:16 +0200

    validations and associations

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,3 +1,7 @@
 class Spaceship < ApplicationRecord
+  has_many :bookings
   belongs_to :user
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :launch_site, presence: true
 end
