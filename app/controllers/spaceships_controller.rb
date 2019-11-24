e8c5cc68964b3ef276023b12846580241b4b60e7
omri shabtay <omrishabtay@gmail.com>
2019-11-24 16:50:57 +0200

    spceships controller

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,11 +1,20 @@
 class SpaceshipsController < ApplicationController
+  before_action :set_spaceship, only: [:show, :edit, :update, :destroy]
   def new
+    @spaceship = Spaceship.new
   end
 
   def index
+    @spaceships = Spaceship.all
   end
 
   def create
+    @spaceship = Spaceship.new(spaceship_params)
+    if @spaceship.save
+      redirect_to @spaceship, notice: 'Spaceship was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,8 +24,22 @@ class SpaceshipsController < ApplicationController
   end
 
   def update
+    @spaceship.update(spaceship_params)
+    redirect_to spaceship_path(@spaceship)
   end
 
   def delete
+    @spaceship.destroy
+    redirect_to spaceships_url, notice: 'Spaceship was successfully destroyed.'
+  end
+
+  private
+
+  def set_spaceship
+    @spaceship = Spaceship.find(params[:id])
+  end
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :cost, :launch_site)
   end
 end
