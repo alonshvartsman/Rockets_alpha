533317f3839208f13b1cd65708c39b228bf7bfae
Ofir Elboym <ofirfir26@gmail.com>
2019-11-24 16:54:58 +0200

    created def dashboard in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    if current_user
+      @bookings = current_user.bookings
+      @spaceships = current_user.spaceships
+    else
+      redirect root_path
+    end
+  end
 end
