cb845418b012b84e3ee9b318001a17fc023fbe33
omri shabtay <omrishabtay@gmail.com>
2019-11-24 17:18:02 +0200

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'dashboard', to: "pages#dashboard"
-  root to: '/spaceships#index'
+  root to: 'spaceships#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :spaceships do
     resources :bookings
