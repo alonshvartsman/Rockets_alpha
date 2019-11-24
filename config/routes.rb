07008f328a0d10c47cf047a1b8002a8d7bcbb795
Ofir Elboym <ofirfir26@gmail.com>
2019-11-24 16:13:58 +0200

    Deleted GET from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'dashboards/index'
-  get 'spaceships/new'
-  get 'spaceships/index'
-  get 'spaceships/create'
-  get 'spaceships/show'
-  get 'spaceships/edit'
-  get 'spaceships/update'
-  get 'spaceships/delete'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
