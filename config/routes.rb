2d59950b330c7c398254b482a33ac38d090ff1b9
Ofir Elboym <ofirfir26@gmail.com>
2019-11-24 15:54:25 +0200

    add spaceship and dashboards controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
+  get 'spaceships/new'
+  get 'spaceships/index'
+  get 'spaceships/create'
+  get 'spaceships/show'
+  get 'spaceships/edit'
+  get 'spaceships/update'
+  get 'spaceships/delete'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
