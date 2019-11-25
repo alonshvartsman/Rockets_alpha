b0e7183ca1df358b3175af56e8b701691ac15ac4
Alon Shvartsman <alonalexandershvartsman@gmail.com>
2019-11-25 18:03:06 +0200

    created 3d plane seating bookings

---
+++ app/javascript/components/plane.js
@@ -0,0 +1,16 @@
+const initPlane = () => {
+  var $body = $('body');
+  console.log($body)
+  $body.on('click','.btn-utility',function(){
+    $('.aircraft-details').toggleClass('open');
+    console.log("clicked")
+  })
+}
+
+export { initPlane }
+
+// const body = document.querySelector("body")
+// const airDet = document.querySelector(".aircraft-details")
+// body.addEventListener("click", (e) => {
+//   airDet.classList.toggle("open")
+// })
