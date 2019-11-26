0d6c8a47fe7a3f1273b347fd594349751fa15a88
Alon Shvartsman <alonalexandershvartsman@gmail.com>
2019-11-26 14:54:02 +0200

    added mapbox map to index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
-import { initPlane } from '../components/plane.js';
 
-initPlane();
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,4 +1,6 @@
 class Spaceship < ApplicationRecord
+  geocoded_by :launch_site
+  after_validation :geocode, if: :will_save_change_to_launch_site?
   has_many :bookings
   belongs_to :user
   validates :name, presence: true
